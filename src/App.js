a572d5323069fd5d8e906cea425b263c4cd27825
Rahul Ranjan <rranjan18bcs@iiitkottayam.ac.in>
2020-07-14 10:36:10 +0530

    moved authentication pages into screen folder

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import { Container } from 'reactstrap'
-import Login from './components/Login/Login'
-import SignUp from './components/SignUp/SignUp'
-import ForgotPassword from './components/ForgotPassword/ForgotPassword'
-import ResetPassword from './components/ResetPassword/ResetPassword'
-import SuccessfulPasswordReset from './components/SuccessfulPasswordReset/SuccessfulPasswordReset'
+
 import Drawer from './components/Drawer/CustomDrawer'
 
 function App() {
   return (
     <Container fluid={true}>
-      {/* <Login/> */}
-      {/* <SignUp /> */}
-      {/* <ResetPassword /> */}
-      {/* <ForgotPassword /> */}
-      {/* <SuccessfulPasswordReset /> */}
       <Drawer />
     </Container>
   );
